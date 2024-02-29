90ff135290122fb161a6959c478ea661f77b0d8f
Karl-Thomas <karl-thomas@zink.ee>
2024-02-29 17:06:46 +0200

    Refactor reshapeDataToAlgorithm and reshapeDataToChart functions

--- src/components/xp-progression.js
+++ src/components/xp-progression.js
@@ -4,21 +4,17 @@ import { transactionProgression } from 'lib/algorithms/transaction-progression-a
 import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import moment from 'moment';
 
-const reshapeDataToAlgorithm = (data) => (
+const reshapeDataToAlgorithm = (data) =>
   data.map((transaction) => {
     const date = new Date(transaction.createdAt);
     return { date, amount: transaction.amount };
-  })
-)
+  });
 
-const reshapeDataToChart = (data) => (
-  data.map((transaction) => (
-    {
-      date: transaction.date,
-      amount: transaction.amount,
-    }
-  ))
-)
+const reshapeDataToChart = (data) =>
+  data.map((transaction) => ({
+    date: transaction.date,
+    amount: transaction.amount,
+  }));
 
 let lastMonth = null;
 const formatXAxis = (tickItem) => {
@@ -34,43 +30,40 @@ const formatXAxis = (tickItem) => {
 };
 
 const CustomTooltip = ({ active, payload, label }) => {
-
   if (active && payload && payload.length) {
     const date = new Date(label);
-    const formattedDate = moment(date).format("DD MMM")
+    const formattedDate = moment(date).format('DD MMM');
     return (
-      <div className="text-foreground">
-        <p className="label">{formattedDate}</p>
-        <p className="label">{`Amount: ${payload[0].value}`}</p>
+      <div className='text-foreground'>
+        <p className='label'>{formattedDate}</p>
+        <p className='label'>{`Amount: ${payload[0].value}`}</p>
       </div>
     );
   }
-}
+};
 
 export default function XpProgression() {
-    const { data } = useQuery({
-        queryKey: ['getTransactions'],
-        queryFn: async () => fetchGraphQL(getTransactions, { order_by: [{ createdAt: "asc"}]} ),
-      });
+  const { data } = useQuery({
+    queryKey: ['getTransactions'],
+    queryFn: async () => fetchGraphQL(getTransactions, { order_by: [{ createdAt: 'asc' }] }),
+  });
 
-    if (data && data.transaction && data.transaction.length > 0) {
-      console.log(data);
-      const reshapedData = reshapeDataToAlgorithm(data.transaction);
-      const results = transactionProgression(reshapedData);
-      const chartData = reshapeDataToChart(results);
+  if (data && data.transaction && data.transaction.length > 0) {
+    console.log(data);
+    const reshapedData = reshapeDataToAlgorithm(data.transaction);
+    const results = transactionProgression(reshapedData);
+    const chartData = reshapeDataToChart(results);
 
-      return (
-        <div className='h-[300px] w-full border border-border'>
-          <ResponsiveContainer height="100%" width="100%">
-            <LineChart width={400} height={300} data={chartData} margin={{ top: 5, right: 20, bottom: 5, left: 20 }}>
-              <Line type="stepBefore" dataKey="amount" stroke="hsl(var(--primary))" dot={false} />
-              <XAxis dataKey="date" interval="preserveEnd" tickFormatter={formatXAxis}/>
-              {/* <YAxis/> */}
-              <Tooltip content={<CustomTooltip />} />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-      )
-
-    }
+    return (
+      <div className='h-[300px] w-full rounded-md border border-border bg-card'>
+        <ResponsiveContainer height='100%' width='100%'>
+          <LineChart data={chartData} margin={{ top: 5, right: 20, bottom: 5, left: 20 }}>
+            <Line type='stepBefore' dataKey='amount' stroke='hsl(var(--primary))' dot={false} />
+            <XAxis dataKey='date' interval='preserveEnd' tickFormatter={formatXAxis} />
+            <Tooltip content={<CustomTooltip />} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
 }
