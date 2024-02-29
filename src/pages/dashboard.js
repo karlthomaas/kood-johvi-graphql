12554954d78a352bf104eb86601cd3351e8b9156
Karl-Thomas <karl-thomas@zink.ee>
2024-02-29 17:01:19 +0200

    Refactor dashboard layout and add navbar

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,19 +1,29 @@
 import XpProgression from 'components/xp-progression';
-import User from 'components/user';
 import SkillsProgression from 'components/skills-progression';
 import { TotalXp } from 'components/total-xp';
 import { AuditsRatio } from 'components/audits-ratio';
+import { Navbar } from 'components/navbar';
 
 export default function Dashboard() {
   return (
-      <div className='max-w-screen-xl flex justify-center'>
-        <div className='p-2 grid grid-cols-1 gap-y-5 mt-2'>
-          <User />
+    <div className='mx-auto flex max-w-screen-lg flex-col'>
+      <div className='mt-2 grid w-full grid-cols-1 gap-5 p-2 sm:grid-cols-2 lg:grid-cols-3'>
+        <div className='sm:col-span-2 lg:order-1 lg:col-span-3'>
+          <Navbar />
+        </div>
+        <div className='sm:col-span-2 lg:order-2'>
           <TotalXp />
+        </div>
+        <div className='sm:col-span-2 lg:order-5 lg:col-span-1'>
           <XpProgression />
-          <SkillsProgression />
+        </div>
+        <div className='sm:col-span-2 lg:order-3 lg:col-span-1'>
           <AuditsRatio />
         </div>
+        <div className='sm:col-span-2 lg:order-6'>
+          <SkillsProgression />
+        </div>
       </div>
+    </div>
   );
 }
