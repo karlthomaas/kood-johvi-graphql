8adf85b8093139f764e342c534f679e04e6b4a79
Karl-Thomas <karl-thomas@zink.ee>
2024-02-29 17:06:54 +0200

    Refactor User component to use div instead of h1

--- src/components/user.js
+++ src/components/user.js
@@ -1,17 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUserInformation, fetchGraphQL } from 'lib/graphql/queries';
 
-
 export default function User() {
-
   const { data } = useQuery({
     queryKey: ['getUserInformation'],
     queryFn: async () => fetchGraphQL(getUserInformation),
   });
 
   if (data) {
-    return (
-        <h1 className='text-3xl tracking-wide'>Welcome, {data.user[0].login}!</h1>
-    );
+    return <div className='text-3xl tracking-wide'>Welcome, {data.user[0].login}!</div>;
   }
 }
